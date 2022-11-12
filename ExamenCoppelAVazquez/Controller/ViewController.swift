1a7d1a2b9cde306a63274d8462fd8824b825fe75
Alejandro <alejandrovcruz0@gmail.com>
2022-11-11 19:50:59 -0600

    Initial Commit

---
+++ ExamenCoppelAVazquez/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExamenCoppelAVazquez
+//
+//  Created by MacBookMBA3 on 11/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ExamenCoppelAVazquez/Model/Movie.swift
@@ -0,0 +1,26 @@
+//
+//  Movie.swift
+//  ExamenCoppelAVazquez
+//
+//  Created by MacBookMBA3 on 11/11/22.
+//
+
+import Foundation
+
+struct pelicula: Codable{
+    var movie: [results]
+}
+
+struct results : Codable{
+    var poster_path: String
+    //var adult: DarwinBoolean
+    //var overview: String
+    //var release_date: String
+    //var genre_ids: Array<Any>
+    //var id: Int
+    //var original_title: String
+    //var original_languaje: String
+    //var title: String
+   // var backdrop_path: String!
+    
+}
